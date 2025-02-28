62e7c53da9dfb42e8bae3db153181a849f0d1bdb
Dowmean <thidarat.fa.65@ubu.ac.th>
2025-02-28 17:24:25 +0700

    อัปเดตตามสไตล์โอนลี่มี นะจ๊ะ

--- grouppro/grouppro/settings.py
+++ grouppro/grouppro/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sklearnapp',
-    'gradioF',
     'teafirst',
     'django_browser_reload',
     'tailwind',
@@ -134,7 +133,8 @@ TEMPLATES = [
     },
 ]
 
-
+TAILWIND_APP_NAME = 'theme'
+NPM_BIN_PATH = r"C:/Program Files/nodejs/npm.cmd"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- grouppro/teafirst/urls.py
+++ grouppro/teafirst/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
+    path('', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     
     path('menu/', menu_view, name='menu'),
